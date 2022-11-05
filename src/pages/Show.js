4b7e3c5f42d3d3aed6c1ca62466e3e3e72c431d4
Gaurav Jadaun <gaurax@hotmail.com>
2022-11-06 01:11:01 +0530

    styled show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,6 +5,7 @@ import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/Config';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 const reducer = (prevState,action) =>{
  switch(action.type){
@@ -68,7 +69,7 @@ return () =>{
  }
 
 
-  return <div>
+  return <ShowPageWrapper>
         <ShowMainData
          image = {show.image}
          name= {show.name}
@@ -77,25 +78,25 @@ return () =>{
             tags = {show.genres}
             />
 
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details
          status = {show.status}
          network = {show.network}
          premiered ={show.premiered}
         />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons = {show._embedded.seasons}/>
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast = {show._embedded.cast}/>
-      </div>
-</div>;
+      </InfoBlock>
+</ShowPageWrapper>;
 }
 
 export default Show
