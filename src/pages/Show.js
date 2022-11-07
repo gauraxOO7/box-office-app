24930abbb6bfb5138278d94a224f581dddd07cec
Gaurav Jadaun <gaurax@hotmail.com>
2022-11-07 19:18:02 +0530

    created two new more hooks : useLastQuery and useShow

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,66 +1,18 @@
-import React,{useEffect,useReducer} from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 import Cast from '../components/show/Cast';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
-import { apiGet } from '../misc/Config';
 import { InfoBlock, ShowPageWrapper } from './Show.styled';
+import { useShow } from '../misc/custom-hooks';
 
-const reducer = (prevState,action) =>{
- switch(action.type){
-  case 'FETCH_SUCCESS':{
-    return {isLoading:false,error:null,show:action.show};
-  }
-
-   case 'FETCH_FAILED':{
-    return {...prevState,isLoading:false,error:action.error};
-   }
-
-    default: 
-   return prevState;
-
- }
-};
-
-const initialState = {
-  show:null,
-  isLoading:true,
-  error:null
-}
 
 const Show = () => {
+ const { id } = useParams();
+ const {show ,isLoading, error} = useShow(id);
 
-const { id } = useParams();
-
- const [{show,isLoading,error}, dispatch] = useReducer(reducer
-  ,initialState)
-
- 
-
-useEffect(() =>{
-let isMounted = true;
-
-apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-.then(results =>{
-      if (isMounted){
-        dispatch({type: 'FETCH_SUCCESS' , show:results})
-      
-    }
-  })
-  .catch( err =>{
-  if (isMounted){
-    dispatch({type: 'FETCH_FAILED' , error:err.message})
-  }
-});
-
-
-return () =>{
-  isMounted =false;
-}
-},[id]);
-
- if(isLoading){
+if(isLoading){
    return <div>Data is being loaded</div>;
  }
 
